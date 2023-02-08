02c0b34efa652398c186739abcb69d282657eb5b
Islam Shadeed <37776598+isllamm@users.noreply.github.com>
2023-02-08 16:10:34 +0200

    TextTsap

---
+++ app/src/main/java/com/isllam/texttsap/MyApp.kt
@@ -0,0 +1,13 @@
+package com.isllam.texttsap
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+    }
+}
